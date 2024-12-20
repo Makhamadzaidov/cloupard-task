85670207da30ec00d36be3d82ef3a902a844392e
Kamronbek X. Maxamadzoidov <k.maxamadzoidov@hlsys.uz>
2024-12-20 15:47:18 +0500

    Updated

--- src/CloupardTask.Mvc/Program.cs
+++ src/CloupardTask.Mvc/Program.cs
@@ -1,7 +1,17 @@
 using CloupardTask.Api.Commons.Helpers;
 using CloupardTask.Api.DbContexts;
 using CloupardTask.Api.Mappers;
+using CloupardTask.DataAccess.Interfaces.Orders;
+using CloupardTask.DataAccess.Repositories.Orders;
+using CloupardTask.Service.Interfaces.Categories;
+using CloupardTask.Service.Interfaces.Commons;
+using CloupardTask.Service.Interfaces.Customers;
+using CloupardTask.Service.Interfaces.Orders;
 using CloupardTask.Service.Interfaces.Products;
+using CloupardTask.Service.Services.Categories;
+using CloupardTask.Service.Services.Commons;
+using CloupardTask.Service.Services.Customers;
+using CloupardTask.Service.Services.Orders;
 using CloupardTask.Service.Services.Products;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,8 +25,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString);
 });
 
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
 builder.Services.AddAutoMapper(typeof(MapperProfile));
+
 builder.Services.AddAuthorization();
 builder.Services.AddControllersWithViews();
 
