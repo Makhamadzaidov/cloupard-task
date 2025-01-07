70fbb63c107f7e14c58aa8ac3edb26585522d3d4
Kamronbek X. Maxamadzoidov <k.maxamadzoidov@hlsys.uz>
2025-01-07 14:42:10 +0500

    Login page

---
+++ src/CloupardTask.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using CloupardTask.Service.Interfaces.Customers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloupardTask.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public AccountController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public IActionResult Login()
+        {
+            return View("~/Views/Account/Login.cshtml");
+        }
+
+    }
+}
