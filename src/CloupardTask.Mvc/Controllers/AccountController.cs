ebac214e20b71dc2bd3885402135e6b0afa27f36
Kamronbek X. Maxamadzoidov <k.maxamadzoidov@hlsys.uz>
2025-01-09 11:00:51 +0500

    some changes

--- src/CloupardTask.Mvc/Controllers/AccountController.cs
+++ src/CloupardTask.Mvc/Controllers/AccountController.cs
@@ -1,24 +1,50 @@
-﻿using CloupardTask.Service.Interfaces.Customers;
+﻿using CloupardTask.Api.Commons.Exceptions;
+using CloupardTask.Service.DTOs.Customers;
+using CloupardTask.Service.Interfaces.Accounts;
+using CloupardTask.Service.Interfaces.Customers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloupardTask.Mvc.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly ICustomerService _customerService;
+        private readonly IAccountService _accountService;
 
-        public AccountController(ICustomerService customerService)
+        public AccountController(IAccountService accountService)
         {
-            _customerService = customerService;
+            _accountService = accountService;
         }
 
-        /*public IActionResult Login()
-        {
-            return View("~/Views/Account/Login.cshtml");
-        }*/
         public ActionResult Index()
         {
             return View("Index");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(CustomerLoginDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.Email) || string.IsNullOrWhiteSpace(dto.Password))
+            {
+                ViewBag.ErrorMessage = "Invalid email or password.";
+                return View("Error");
+            }
+
+            var user = await _accountService.LoginAsync(dto);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = "Invalid email or password.";
+                return View("Error");
+            }
+
+            if (dto.Email == "admin@gmail.com" && dto.Password == "1234")
+            {
+                return RedirectToAction("AdminDashboard", "Admin");
+            }
+
+            return View("Index", user);
+        }
+
+
     }
 }
